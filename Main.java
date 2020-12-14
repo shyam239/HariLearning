7ca7376b3830b2b419d4d2f03a52dc030a350221
ShyamKids <shyam.tandem@gmail.com>
2020-12-14 01:23:55 +0000

    Calling methods from main method

--- Main.java
+++ Main.java
@@ -10,19 +10,45 @@ class Main {
     
     // add two values and print the output
     outPut = valOne+valTwo;
-    System.out.println("Addition of two values = "+outPut);
+    System.out.println("Addition of two values, from main method = "+outPut);
 
     // sub two values and print the output
     outPut = valOne-valTwo;
-    System.out.println("Subracation of two values = "+outPut);
+    System.out.println("Subracation of two values, from main method = "+outPut);
 
     // multiply two values and print the output
     outPut = valOne*valTwo;
-    System.out.println("Multiplication of two values = "+outPut);
+    System.out.println("Multiplication of two values, from main method = "+outPut);
 
     // divid two values and print the output
     outPut = valOne/valTwo;
-    System.out.println("Division of two values = "+outPut);
+    System.out.println("Division of two values, from main method = "+outPut);
+
+    addTwoValAndPrint(6, 3);
+    subTwoValAndPrint(6, 3);
+    mulTwoValAndPrint(6, 3);
+    divTwoValAndPrint(6, 3);
 
   }
+
+  public static void addTwoValAndPrint(int valOne, int valTwo){
+    int outPut=valOne+valTwo;
+    System.out.println("Addition of two values, printing from addTwoValAndPrint method = "+outPut);
+  }
+
+  public static void subTwoValAndPrint(int valOne, int valTwo){
+    int outPut=valOne-valTwo;
+    System.out.println("Subracation of two values, printing from subTwoValAndPrint method = "+outPut);
+  }
+
+  public static void mulTwoValAndPrint(int valOne, int valTwo){
+    int outPut=valOne*valTwo;
+    System.out.println("Multiplication of two values, printing from mulTwoValAndPrint method = "+outPut);
+  }
+
+   public static void divTwoValAndPrint(int valOne, int valTwo){
+    int outPut=valOne/valTwo;
+    System.out.println("Division of two values, printing from divTwoValAndPrint method = "+outPut);
+  }
+
 }
