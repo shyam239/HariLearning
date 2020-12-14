a9510f0d0833151a8d361f39aa93ef31723233cb
ShyamKids <shyam.tandem@gmail.com>
2020-12-14 01:00:58 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,28 @@
+class Main {
+  public static void main(String[] args) {
+    // declaration of variables
+    int valOne;
+    int valTwo;
+    int outPut;
+    // setting values in variables in input variables
+    valOne=8;
+    valTwo=2;
+    
+    // add two values and print the output
+    outPut = valOne+valTwo;
+    System.out.println("Addition of tow values = "+outPut);
+
+    // sub two values and print the output
+    outPut = valOne-valTwo;
+    System.out.println("Subracation of tow values = "+outPut);
+
+    // multiply two values and print the output
+    outPut = valOne*valTwo;
+    System.out.println("Multiplication of tow values = "+outPut);
+
+    // divid two values and print the output
+    outPut = valOne/valTwo;
+    System.out.println("Division of tow values = "+outPut);
+
+  }
+}
